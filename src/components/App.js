7d784411adf4ebf8831b1ac4aaf3a2954764cf1a
edwardosindo <eddu.almasi@gmail.com>
2022-02-25 22:39:48 +0300

    created post

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Web3 from 'web3';
 
 import Identicon from 'identicon.js';
 import Navbar from './Navbar';
+import Main from './Main';
 
 class App extends Component {
   // Now we call the function above wheneever the react component is loaded
@@ -57,6 +58,7 @@ class App extends Component {
         })
         console.log({ posts: this.state.posts })
       }
+      this.setState({loading: false })
     } else {
       window.alert('SocialNetwork contract not deployed to the detected network')
     }
@@ -64,60 +66,41 @@ class App extends Component {
     // ABI
   }
 
+  createPost(content) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  };
+
   constructor(props) {
     super(props)
     this.state = {
       account: '',
       socialNetwork: null,
       postCount: 0, 
-      posts: []
+      posts: [],
+      loading: true
+      
     }
+
+    this.createPost  = this.createPost.bind(this)
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account}/>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px'}}>
-              <div className="content mr-auto ml-auto">
-                {/*loop through all the posts */}
-                { this.state.posts.map((post, key) => {
-                  return (
-                    <div className="card mb-4" key={key}>
-                      <div className= "card-header">
-                        <img 
-                              className='mr-2'
-                              width='30'
-                              height='30'
-                              src={`data:image/png;base64,${new Identicon(this.state.account, 30).toString()}`}
-                              alt=""
-                        />
-                        <small className="text-muted">{post.author}</small>
-                      </div>
-                      <ul id="postList" className="list-group list-group-flush">
-                        <li className="list-group-item">
-                          <p>{post.content}</p>
-                        </li>
-                        <li key={key} className="list-group-item py-2">
-                          <small className="float-left mt-1 text-muted">
-                            TIPS: {window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')} ETH
-                          </small>
-                          <button className="btn btn-link btn-sm float-right pt-0">
-                            <span>
-                              TIP 0.1 ETH
-                            </span>
-                          </button>
-                        </li>
-                      </ul>
-                    </div>
-                  )
-                })}
-              </div>
-            </main>
-          </div>
-        </div>
+        {this.state.loading 
+          ? <div id="loader" className="text-center mt-5"><p>loading....</p></div>
+          : <Main 
+              posts ={this.state.posts}
+              createPost = { this.createPost }
+            />
+        }
+
+
       </div>
     );
   }
